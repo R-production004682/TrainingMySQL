956d598ab605fc7c4099ed44506a0ed83a7ddeb2
Kr004682zero@icloud.com <Kr004682zero@icloud.com>
2024-07-26 00:37:50 +0900

    UPDATE,DELETE文の追加

--- Training_6.sql
+++ Training_6.sql
@@ -15,3 +15,21 @@ select name as '名前' , birth_day as '誕生日' from people where id = 1;
 
 #名前の検索もできます。
 select * from people where name = 'Taro';
+
+#UPDATE文
+update people set birth_day = "1900-01-01" , name = "";
+select * from people;
+
+#UPDATE where
+update people set name = "Yamada" , birth_day = "2000-01-01" where id = 2;
+update people set name = "Taro" , birth_day = "2000-01-01" where id = 3;
+
+#DELETE レコード削除
+DELETE from people where id = 2;
+
+select * from people;
+
+#DELETE 全削除
+delete from people;
+
+select * from people;
