c6ae8495877b9c6565caed592e2676bdb36369ff
Kr004682zero@icloud.com <Kr004682zero@icloud.com>
2024-08-26 01:37:11 +0900

    テーブルに別名を付けて参照する

--- Training_2_7.sql
+++ Training_2_7.sql
@@ -3,3 +3,12 @@ show databases;
 create database my_db2;
 
 use my_db2;
+
+#テーブルにcsという別の名前を付ける
+select cs.name from classes as cs;
+
+#テーブルに別名を付けるわけなので、複数のカラムを指定することが出来る。
+select cs.name,cs.id from classes as cs;
+
+#デフォルトの名前も使用可
+select cs.name,id from classes as cs;
